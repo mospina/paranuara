8f2184aaefef963378a7453256c1892f2402c149
Manuel Ospina <manuel@anect.jp>
2020-01-07 09:28:57 +0900

    Add guards

--- core/management/commands/load_data.py
+++ core/management/commands/load_data.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from companies.models import Company
 from people.loader import load_data_from_file
@@ -6,12 +6,17 @@ from people.loader import load_data_from_file
 
 class Command(BaseCommand):
     help = "Read data from a json file and store it in the database"
+    usage = "usage: python manage.py load_data --companies companies.json --people people.json"
 
     def add_arguments(self, parser):
         parser.add_argument("--companies", nargs="+", dest="companies", type=str)
         parser.add_argument("--people", nargs="+", dest="people", type=str)
 
     def handle(self, *args, **options):
+        if not options.get("companies") or not options.get("people"):
+            self.stdout.write(self.usage)
+            return
+
         # First load companies
         for elem in options["companies"]:
             Company.load_data_from_file(elem)
